104ac4846dce1d67091b4e53c3d768c6badfa648
Tipuch <fiorini751@proton.me>
2025-05-04 20:37:38 -0400

    added event function, WIP

--- src/tile.rs
+++ src/tile.rs
@@ -1,8 +1,12 @@
-use bevy::{prelude::*, window::PrimaryWindow};
-use bevy_ecs_tiled::{
-    map::TiledMapHandle,
-    prelude::{TiledMap, TiledMapAnchor},
+use bevy::prelude::*;
+use bevy::{
+    color::palettes::css::{PURPLE, RED},
+    ecs::{component::ComponentId, world::DeferredWorld},
+    prelude::*,
 };
+use bevy_ecs_tiled::prelude::*;
+use bevy_ecs_tiled::prelude::*;
+use bevy_ecs_tilemap::prelude::*;
 use bevy_quadtree::CollisionRect;
 
 pub const TILE_SIZE: f32 = 32.0;
@@ -27,6 +31,29 @@ pub fn get_tile_to_world(tile_pos: IVec2, window: &Window) -> Vec2 {
     let world_x = (tile_pos.x as f32 * TILE_SIZE) + (TILE_SIZE / 2.0) - offset.x;
     let world_y = (tile_pos.y as f32 * TILE_SIZE) + (TILE_SIZE / 2.0) - offset.y;
     Vec2::new(world_x, world_y)
+}
+
+// A typical usecase for regular events is to update components associated with tiles, objects or layers.
+// Here, we will add a small offset on the Z axis to our objects to prevent them
+// from Z-fighting if they are on the same layer (by default, all objects on a given layer have the same Z offset)
+fn evt_object_created(
+    mut object_events: EventReader<TiledObjectCreated>,
+    mut object_query: Query<(&Name, &mut Transform), With<TiledMapObject>>,
+    mut z_offset: Local<f32>,
+) {
+    for e in object_events.read() {
+        let Ok((name, mut transform)) = object_query.get_mut(e.entity) else {
+            return;
+        };
+
+        info!("=> Received TiledObjectCreated event for object '{}'", name);
+
+        // Obviously, this is a very naive implementation and you would
+        // probably want to do something else in a real usecase
+        info!("Apply z-offset = {:?}", *z_offset);
+        transform.translation.z += *z_offset;
+        *z_offset += 0.01;
+    }
 }
 
 // TODO add resource to get the tile map limits max_x and max_y min_x min_y such that the camera
